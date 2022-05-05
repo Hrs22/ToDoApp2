5c8b745f2b2adbc71e24537c73a79af235755314
resrvationmanager <sonity.main@gmail.com>
2022-05-05 11:23:16 +0300

    Added login controller, added entity framework

--- ToDoList2/Controller/LoginController.cs
+++ ToDoList2/Controller/LoginController.cs
@@ -1,51 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ToDoList2.Model;
+﻿using ToDoList2.Model;
 
 namespace ToDoList2.Controller
 {
-     class LoginController
+    public class LoginController
     {
 
         public bool IsUserLogged(string username, string password)
         {
-            using(Enti log = new RegisterModel())
+            using(RegisterDBEntities log = new RegisterDBEntities())
             {
                 bool isLogged = false;
-                foreach (var user in log.)
+                foreach (var user in log.RegisterTables)
                 {
-
+                    if (user.username == username && user.password == password)
+                    {
+                        isLogged = true;
+                        GlobalData.CurrentUserId = user.Id;
+                    }
                 }
-            }
-        }
 
-
-        /* public List<RegisterTable> GetAll()
-        {
-            using (RegisterModel model = new RegisterModel())
-            {
-                return model.RegisterTables.ToList();
+                return isLogged;
             }
         }
-        public List<RegisterTable> ShowAll()
-        {
-            using (RegisterModel reg = new RegisterModel())
-            {
-                var register = reg.RegisterTables.ToList();
-                return register;
-            }
-        }
-        public void CreateRegister(RegisterTable r)
-        {
-            using (RegisterModel reg = new RegisterModel())
-            {
-                r.Id = reg.RegisterTables.ToList().LastOrDefault().Id + 1;
-                reg.RegisterTables.Add(r);
-                reg.SaveChanges();
-            }
-        }*/
     }
 }
